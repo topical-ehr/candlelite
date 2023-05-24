4b2036144ddbcd19bd843eab4e53eacfb7db4cf2
Eugene Lubarsky <eug48@users.noreply.github.com>
2023-05-24 13:14:20 +1000

    more search parameters

--- CandleLite.Core/SearchParameters.fs
+++ CandleLite.Core/SearchParameters.fs
@@ -26,6 +26,18 @@ let parameters =
             "name", humanName [ "name" ]
         ]
 
+        "Composition",
+        [
+            identifier
+
+            "type", codeableConcept "type"
+            "category", codeableConcept "category"
+            "status", indexString ["status"]
+
+            reference "encounter"
+            reference "subject"
+        ]
+
         "Condition",
         [
             identifier
@@ -34,6 +46,29 @@ let parameters =
             "category", codeableConcept "category"
             "verification-status", codeableConcept "verificationStatus"
             "clinical-status", codeableConcept "clinicalStatus"
+
+            reference "encounter"
+            reference "subject"
+        ]
+
+        "DiagnosticReport",
+        [
+            identifier
+
+            "code", codeableConcept "code"
+            "category", codeableConcept "category"
+            "status", indexString [ "status" ]
+
+            reference "encounter"
+            reference "subject"
+        ]
+
+        "MedicationRequest",
+        [
+            identifier
+
+            "category", codeableConcept "category"
+            "status", indexString ["status"]
 
             reference "encounter"
             reference "subject"
