aca94a98ee1cbb4b1139f772a6532bfa5df04d7b
Eugene Lubarsky <eug48@users.noreply.github.com>
2023-07-08 00:42:02 +1000

    server: one request a time to so it's easier to read logs

--- CandleLite.DotNet.SampleServer/AspNetCoreServer.cs
+++ CandleLite.DotNet.SampleServer/AspNetCoreServer.cs
@@ -3,18 +3,13 @@ namespace CandleLite.DotNet.SampleServer;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Logging;
 
-using LotusLogger;
+using LMLogger;
 
 using CandleLite.Core;
 using CandleLite.DotNet;
 
 public class AspNetCoreServer
 {
-    // Initialise CandleLite
-    static readonly SQLite.DotNetSQLiteImpl dbImpl = SQLite.DotNetSQLiteImpl.UseFile("CandleLite.sqlite.db");
-    static readonly Server.ICandleLiteJSON jsonImpl = new JsonViaJsonNode.DotNetJSON(indent: true);
-    static readonly Server.ICandleLiteServer fhirServer = new Server.CandleLiteServer(new CandleLiteConfig(), dbImpl, jsonImpl);
-
     public static void Main(string[] args)
     {
         Run(args, 5454, CancellationToken.None).Wait();
@@ -26,10 +21,13 @@ public class AspNetCoreServer
         builder.Services.AddHttpClient();
         var app = builder.Build();
 
+        app.UseDeveloperExceptionPage();
+        app.UseStatusCodePages();
+        app.MapGet("/", () => $"Hello!\nCandleLite sample FHIR server is running from {Environment.CurrentDirectory}");
 
-        // initialise logging
-        Logger.Sink = new LotusLogger.Sinks.HttpSink(
-            new Uri("http://localhost:10000/log/demo"),
+        // Initialise logging
+        Logger.Sink = new LMLogger.Sinks.HttpSink(
+            new Uri("http://localhost:10000/log/CandleLite"),
             app.Services.GetRequiredService<IHttpClientFactory>(),
             TimeSpan.FromSeconds(1),
             app.Services.GetRequiredService<ILoggerFactory>()
@@ -37,42 +35,53 @@ public class AspNetCoreServer
         Logger.Component = "CandleLite";
         Logger.InstanceId = Guid.NewGuid().ToString();
 
-        app.Urls.Add($"http://*:{port}");
+        // Initialise CandleLite
+        Server.ICandleLiteDB dbImpl = SQLite.DotNetSQLiteImpl.UseFile("CandleLite.sqlite.db");
+        Server.ICandleLiteJSON jsonImpl = new JsonViaJsonNode.DotNetJSON(indent: true);
+        Server.ICandleLiteServer fhirServer = new Server.CandleLiteServer(new CandleLiteConfig(), dbImpl, jsonImpl);
 
-        app.UseDeveloperExceptionPage();
-        app.UseStatusCodePages();
-
-        app.MapGet("/", () => $"Hello!\nCandleLite sample FHIR server is running from {Environment.CurrentDirectory}");
+        // do one request a time to so it's easier to read logs
+        var requestLimiter = new SemaphoreSlim(1);
 
         app.MapMethods(
             "/fhir/{*path}",
             new[] { "GET", "POST", "PUT", "DELETE" },
             async (HttpRequest req, HttpResponse res) =>
             {
-                string bodyString = await new StreamReader(req.Body).ReadToEndAsync();
-
-                var response = fhirServer.HandleRequest(
-                    req.Method.ToString(),
-                    req.GetEncodedPathAndQuery(),
-                    "/fhir",
-                    bodyString,
-                    header => req.Headers[header].ToString(),
-                    (header, value) => res.Headers[header] = value
-                );
-
-                if (response.Status == 204)
+                await requestLimiter.WaitAsync();
+                try
                 {
-                    return Results.NoContent();
+                    string bodyString = await new StreamReader(req.Body).ReadToEndAsync();
+
+                    var response = fhirServer.HandleRequest(
+                        req.Method.ToString(),
+                        req.GetEncodedPathAndQuery(),
+                        "/fhir",
+                        bodyString,
+                        header => req.Headers[header].ToString(),
+                        (header, value) => res.Headers[header] = value
+                    );
+
+                    if (response.Status == 204)
+                    {
+                        return Results.NoContent();
+                    }
+                    else
+                    {
+                        res.StatusCode = response.Status;
+                        return Results.Text(response.BodyString, "application/fhir+json");
+                    }
                 }
-                else
+                finally
                 {
-                    res.StatusCode = response.Status;
-                    return Results.Text(response.BodyString, "application/fhir+json");
+                    requestLimiter.Release();
                 }
             }
         );
 
         BrowseInHtml.AddRoutes(app, fhirServer);
+
+        app.Urls.Add($"http://*:{port}");
 
         return app.RunAsync(ct);
     }
